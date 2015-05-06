0a4b5dcfd34678ad8d9757ff80d11f93c2418953
Anthony Mattox <anthony@friendsoftheweb.com>
2015-05-06 14:20:24 -0400

    Initial Commit

---
+++ Gradients/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gradients
+//
+//  Created by Anthony Mattox on 5/6/15.
+//  Copyright (c) 2015 Friends of The Web. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GradientsTests/GradientsTests.m
@@ -0,0 +1,40 @@
+//
+//  GradientsTests.m
+//  GradientsTests
+//
+//  Created by Anthony Mattox on 5/6/15.
+//  Copyright (c) 2015 Friends of The Web. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GradientsTests : XCTestCase
+
+@end
+
+@implementation GradientsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
